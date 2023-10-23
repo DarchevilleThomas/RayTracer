c3c384d1e0bb470483608061b9a89cf26a440920
Gregory <gregory>
2023-10-23 13:39:56 +0200

    Constructor of Color class

---
+++ src/main/java/fr/saeS3A01/group1/Color.java
@@ -0,0 +1,20 @@
+package fr.saeS3A01.group1;
+
+public class Color {
+
+    private Triplet triplet;
+
+    private double red, green, blue;
+    public Color(double red, double green, double blue,double x, double y, double z){
+        triplet = new Triplet(x,y,z);
+        this.red=red;
+        this.green=green;
+        this.blue=blue;
+    }
+
+    public Color(double x, double y, double z){
+        this(x,y,z,0.0,0.0,0.0);
+    }
+
+
+}
