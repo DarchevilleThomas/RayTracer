f1fc60eca27f22f1673b953acb72b2eded28d12c
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 13:56:52 +0200

    Add setter and getter to class DirectionalLight

--- src/main/java/fr/saeS3A01/group1/DirectionalLight.java
+++ src/main/java/fr/saeS3A01/group1/DirectionalLight.java
@@ -10,4 +10,13 @@ class DirectionalLight extends Light {
         super(position, intensity);
         this.direction = direction;
     }
+
+    // Getter and Setter methods for direction
+    public Vector getDirection() {
+        return this.direction;
+    }
+
+    public void setDirection(Vector direction) {
+        this.direction = direction;
+    }
 }
