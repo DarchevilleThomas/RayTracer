71392d00cdda81cc82b713ad62888788e1a040bb
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 13:08:40 +0200

    Replacing comments with docstring

--- src/main/java/fr/saeS3A01/group1/Triplet.java
+++ src/main/java/fr/saeS3A01/group1/Triplet.java
@@ -1,56 +1,93 @@
 package fr.saeS3A01.group1;
 
 /**
- * An abstract class to represent a 3-dimensional tuple (point, vector, color)
+ * This class represents a 3-dimensional tuple (point, vector, color).
+ * The attributes x, y and z are protected to be accessible by subclasses.
  */
 class Triplet {
-    // The attributes x, y and z are protected to be accessible by subclasses
     protected double x;
     protected double y;
     protected double z;
 
-    // The constructor takes three real parameters
+    /**
+     * Constructs a new Triplet with the given x, y and z values.
+     *
+     * @param x The x value.
+     * @param y The y value.
+     * @param z The z value.
+     */
     public Triplet(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    // The following methods are common to all triplets
-    // They should be overridden in subclasses if necessary
-    // Method to compute the sum of two triplets
+    /**
+     * Computes the sum of this triplet and another triplet.
+     *
+     * @param t The other triplet.
+     * @return A new triplet that is the sum of this triplet and the other triplet.
+     */
     public Triplet add(Triplet t) {
         return new Triplet(this.x + t.x, this.y + t.y, this.z + t.z);
     }
 
-    // Method to compute the difference of two triplets
+    /**
+     * Computes the difference of this triplet and another triplet.
+     *
+     * @param t The other triplet.
+     * @return A new triplet that is the difference of this triplet and the other triplet.
+     */
     public Triplet sub(Triplet t) {
         return new Triplet(this.x - t.x, this.y - t.y, this.z - t.z);
     }
 
-    // Method to compute the product by a scalar
+    /**
+     * Computes the product of this triplet by a scalar.
+     *
+     * @param d The scalar.
+     * @return A new triplet that is the product of this triplet and the scalar.
+     */
     public Triplet mul(double d) {
         return new Triplet(this.x * d, this.y * d, this.z * d);
     }
 
-    // Method to compute the dot product with another triplet
+    /**
+     * Computes the dot product of this triplet with another triplet.
+     *
+     * @param t The other triplet.
+     * @return The dot product of this triplet and the other triplet.
+     */
     public double dot(Triplet t) {
         return this.x * t.x + this.y * t.y + this.z * t.z;
     }
 
-    // Method to compute the cross product with another triplet
+    /**
+     * Computes the cross product of this triplet with another triplet.
+     *
+     * @param t The other triplet.
+     * @return A new triplet that is the cross product of this triplet and the other triplet.
+     */
     public Triplet cross(Triplet t) {
         return new Triplet(this.y * t.z - this.z * t.y,
                 this.z * t.x - this.x * t.z,
                 this.x * t.y - this.y * t.x);
     }
 
-    // Method to compute the length of the triplet
+    /**
+     * Computes the length of this triplet.
+     *
+     * @return The length of this triplet.
+     */
     public double length() {
         return Math.sqrt(this.dot(this));
     }
 
-    // Method to normalize the triplet (make it unitary)
+    /**
+     * Normalizes this triplet (makes it unitary).
+     *
+     * @return A new normalized version of this triplet if it's not null; otherwise returns itself.
+     */
     public Triplet normalize() {
         double l = this.length();
         if (l == 0) {
