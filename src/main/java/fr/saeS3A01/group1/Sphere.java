a0dea1b3f0fc30075a79a056da63a35a8e601d05
Noah <hemery.noah18@gmail.com>
2023-10-23 17:13:52 +0200

    Added the sphere class and the sphere constructor

---
+++ src/main/java/fr/saeS3A01/group1/Sphere.java
@@ -0,0 +1,28 @@
+package fr.saeS3A01.group1;
+
+public class Sphere extends Shape {
+
+    private Color diffuse;
+    private Color specular;
+    private int shininess;
+    private double radius;
+    private Point p;
+
+    /**
+     * Constructor of a sphere
+     * @param diffuse a color
+     * @param specular a color
+     * @param shininess a int
+     * @param p a point
+     * @param radius a double
+     */
+    public Sphere(Color diffuse, Color specular, int shininess,Point p,double radius) {
+        super(diffuse, specular, shininess);
+        this.p = p;
+        this.radius = radius;
+    }
+
+
+
+
+}
