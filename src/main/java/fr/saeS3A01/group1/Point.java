89f1969bfd010d8dc33e2f98140df4ba6fcb2101
Thomas <darcheville.toma@gmail.com>
2023-10-23 14:29:11 +0200

    Add docstring

--- src/main/java/fr/saeS3A01/group1/Point.java
+++ src/main/java/fr/saeS3A01/group1/Point.java
@@ -3,28 +3,62 @@ package fr.saeS3A01.group1;
 public class Point {
     private Triplet triplet;
 
+    /**
+     * Constructor with variables x,y,z
+     *
+     * @param x a double
+     * @param y a double
+     * @param z a double
+     */
     public Point(double x, double y, double z){
         this.triplet=new Triplet(x,y,z);
     }
 
+    /**
+     * Constructor with a triplet
+     *
+     * @param t a triplet
+     */
     public Point(Triplet t){
         this.triplet = t;
     }
 
+    /**
+     * Subtraction of 2 points returning a vector
+     *
+     * @param p (Point)
+     * @return Vector
+     * @throws Exception If the parameter p is null or the triplet is null, the method throws an exception
+     */
     public Vector substract(Point p) throws Exception {
         if (p==null || this.triplet==null) throw new Exception("Impossible de soustraire un triplet vide");
         return new Vector(this.triplet.sub(p.getTriplet()));
     }
 
+    /**
+     * Multiplication by a scalar of a point
+     *
+     * @param d a double
+     * @return a point
+     * @throws Exception If the triplet is null, the method throws an exception
+     */
     public Point mul(double d) throws Exception {
         if (this.triplet==null) throw new Exception("Impossible de multiplié un triplet null par un produit scalaire");
         return new Point(this.triplet.mul(d));
     }
 
+    /**
+     *
+     * @return a point
+     */
     public Triplet getTriplet() {
         return triplet;
     }
 
+    /**
+     *
+     * @param triplet a triplet
+     */
     public void setTriplet(Triplet triplet) {
         this.triplet = triplet;
     }
