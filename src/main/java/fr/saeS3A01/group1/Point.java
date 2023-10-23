d1140e7ead961667800fc3642b8e3ad3671a0d27
Thomas <darcheville.toma@gmail.com>
2023-10-23 13:54:05 +0200

    Add constructors and substract method

--- src/main/java/fr/saeS3A01/group1/Point.java
+++ src/main/java/fr/saeS3A01/group1/Point.java
@@ -3,4 +3,19 @@ package fr.saeS3A01.group1;
 public class Point {
     private Triplet triplet;
 
+    public Point(double x, double y, double z){
+        this.triplet=new Triplet(x,y,z);
+    }
+
+    public Point(Triplet t){
+        this.triplet = t;
+    }
+
+    public Vector substract(Point p) throws Exception {
+        if (p==null || this.triplet==null) throw new Exception("Impossible de soustraire un triplet vide");
+        return new Vector(this.triplet.sub(p.getTriplet()));
+    }
+
+
+
 }
