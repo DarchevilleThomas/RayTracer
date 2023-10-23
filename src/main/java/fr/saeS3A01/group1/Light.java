3177dcd1e8b01e656abea1d3b4e193fbb1947ec6
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 13:54:30 +0200

    Adding attributes and a constructor to class Light

--- src/main/java/fr/saeS3A01/group1/Light.java
+++ src/main/java/fr/saeS3A01/group1/Light.java
@@ -4,5 +4,11 @@ package fr.saeS3A01.group1;
  * Light class that represents a light source.
  */
 class Light {
-    // TODO
+    protected Point position;
+    protected Color intensity;
+
+    public Light(Point position, Color intensity) {
+        this.position = position;
+        this.intensity = intensity;
+    }
 }
