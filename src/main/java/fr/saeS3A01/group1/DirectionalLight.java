36973eea6743bbf38693a098946062cdbf368b48
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 13:56:19 +0200

    Adding attributes and a constructor to class DirectionalLight

--- src/main/java/fr/saeS3A01/group1/DirectionalLight.java
+++ src/main/java/fr/saeS3A01/group1/DirectionalLight.java
@@ -1,5 +1,13 @@
 package fr.saeS3A01.group1;
 
-public class DirectionalLight {
-    
+/**
+ * DirectionalLight class that represents a light source with a specific direction.
+ */
+class DirectionalLight extends Light {
+    protected Vector direction;
+
+    public DirectionalLight(Point position, Color intensity, Vector direction) {
+        super(position, intensity);
+        this.direction = direction;
+    }
 }
