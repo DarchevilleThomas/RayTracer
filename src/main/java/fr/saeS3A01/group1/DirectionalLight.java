29dee133aaadb6b97dface319c3e48c4686b242a
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 14:38:40 +0200

    Adding docstring to the DirectionalLight class

--- src/main/java/fr/saeS3A01/group1/DirectionalLight.java
+++ src/main/java/fr/saeS3A01/group1/DirectionalLight.java
@@ -1,21 +1,41 @@
 package fr.saeS3A01.group1;
 
 /**
- * DirectionalLight class that represents a light source with a specific direction.
+ * This class represents a directional light source.
+ * It extends the Light class and adds a specific direction.
  */
 class DirectionalLight extends Light {
+    /**
+     * The direction of the light source.
+     */
     protected Vector direction;
 
+    /**
+     * Constructs a new DirectionalLight with the given position, intensity, and direction.
+     *
+     * @param position The position of the light source.
+     * @param intensity The intensity of the light source.
+     * @param direction The direction of the light source.
+     */
     public DirectionalLight(Point position, Color intensity, Vector direction) {
         super(position, intensity);
         this.direction = direction;
     }
 
-    // Getter and Setter methods for direction
+    /**
+     * Returns the direction of this light source.
+     *
+     * @return The direction of this light source.
+     */
     public Vector getDirection() {
         return this.direction;
     }
 
+    /**
+     * Sets the direction of this light source to the given direction.
+     *
+     * @param direction The new direction of this light source.
+     */
     public void setDirection(Vector direction) {
         this.direction = direction;
     }
