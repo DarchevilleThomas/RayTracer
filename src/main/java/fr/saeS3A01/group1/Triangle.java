cd2d75fea917b9740f9786050574bab67ece592d
Darcheville Thomas <thomas_darcheville@ens.univ-artois.fr>
2023-10-23 16:58:59 +0000

    Update javadoc of Triangle class

--- src/main/java/fr/saeS3A01/group1/Triangle.java
+++ src/main/java/fr/saeS3A01/group1/Triangle.java
@@ -12,6 +12,9 @@ public class Triangle extends Shape{
      * @param diffuse   a Color
      * @param specular  a Color
      * @param shininess a int
+     * @param point1 a Point
+     * @param point2 a Point
+     * @param point3 a Point
      */
     public Triangle(Color diffuse, Color specular, int shininess, Point point1, Point point2, Point point3) {
         super(diffuse, specular, shininess);
