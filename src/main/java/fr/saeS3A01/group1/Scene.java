1fa37cd798f70a7d6aede883419b3f75453dd081
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 16:46:01 +0200

    Adding Scene class attributes

--- src/main/java/fr/saeS3A01/group1/Scene.java
+++ src/main/java/fr/saeS3A01/group1/Scene.java
@@ -2,5 +2,11 @@ package fr.saeS3A01.group1;
 
 // Une classe pour représenter une scène 3D
 class Scene {
-    // TODO
+    // Les attributs de la scène
+    private Camera camera; // La caméra qui capture la scène
+    private int width; // La largeur de l'image en pixels
+    private int height; // La hauteur de l'image en pixels
+    private ArrayList<Light> lights; // La liste des lumières dans la scène
+    private ArrayList<Shape3D> shapes; // La liste des formes 3D dans la scène
+
 }
