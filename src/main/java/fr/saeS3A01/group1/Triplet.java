c0c9b3c8a41d182f9d17e5b14e807fce170fff1e
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-23 13:06:30 +0200

    Switch comments language to english

--- src/main/java/fr/saeS3A01/group1/Triplet.java
+++ src/main/java/fr/saeS3A01/group1/Triplet.java
@@ -1,60 +1,62 @@
 package fr.saeS3A01.group1;
 
-// Définir une classe abstraite Triplet pour représenter un tuple de dimension 3
+/**
+ * An abstract class to represent a 3-dimensional tuple (point, vector, color)
+ */
 class Triplet {
-    // Les attributs x, y et z sont protégés pour être accessibles aux classes filles
+    // The attributes x, y and z are protected to be accessible by subclasses
     protected double x;
     protected double y;
     protected double z;
 
-    // Le constructeur prend trois paramètres réels
+    // The constructor takes three real parameters
     public Triplet(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    // Les méthodes suivantes sont communes à tous les triplets
-    // Elles doivent être redéfinies dans les classes filles si nécessaire
-    // Méthode pour calculer la somme de deux triplets
+    // The following methods are common to all triplets
+    // They should be overridden in subclasses if necessary
+    // Method to compute the sum of two triplets
     public Triplet add(Triplet t) {
         return new Triplet(this.x + t.x, this.y + t.y, this.z + t.z);
     }
 
-    // Méthode pour calculer la différence de deux triplets
+    // Method to compute the difference of two triplets
     public Triplet sub(Triplet t) {
         return new Triplet(this.x - t.x, this.y - t.y, this.z - t.z);
     }
 
-    // Méthode pour calculer le produit par un scalaire
+    // Method to compute the product by a scalar
     public Triplet mul(double d) {
         return new Triplet(this.x * d, this.y * d, this.z * d);
     }
 
-    // Méthode pour calculer le produit scalaire avec un autre triplet
+    // Method to compute the dot product with another triplet
     public double dot(Triplet t) {
         return this.x * t.x + this.y * t.y + this.z * t.z;
     }
 
-    // Méthode pour calculer le produit vectoriel avec un autre triplet
+    // Method to compute the cross product with another triplet
     public Triplet cross(Triplet t) {
         return new Triplet(this.y * t.z - this.z * t.y,
                 this.z * t.x - this.x * t.z,
                 this.x * t.y - this.y * t.x);
     }
 
-    // Méthode pour calculer la longueur du triplet
+    // Method to compute the length of the triplet
     public double length() {
         return Math.sqrt(this.dot(this));
     }
 
-    // Méthode pour normaliser le triplet (le rendre unitaire)
+    // Method to normalize the triplet (make it unitary)
     public Triplet normalize() {
         double l = this.length();
         if (l == 0) {
-            return this; // On ne peut pas normaliser un triplet nul
+            return this; // Cannot normalize a null triplet
         } else {
-            return this.mul(1 / l); // On divise par la longueur
+            return this.mul(1 / l); // Divide by the length
         }
     }
 }
