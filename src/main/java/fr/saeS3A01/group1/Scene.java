944b15d1720a678315cdcb49fa33470918db0e04
Gregory <gregory>
2023-10-24 09:47:32 +0200

    adding javadoc

--- src/main/java/fr/saeS3A01/group1/Scene.java
+++ src/main/java/fr/saeS3A01/group1/Scene.java
@@ -9,6 +9,12 @@ public class Scene {
     private ArrayList<Light> lights;
     private ArrayList<Shape> shapes;
 
+    /**
+     * Constructor for Scene class
+     * @param camera a Camera
+     * @param width a int
+     * @param height a int
+     */
     public Scene(Camera camera, int width, int height) {
         this.camera = camera;
         this.width = width;
@@ -17,50 +23,99 @@ public class Scene {
         this.shapes = new ArrayList<Shape>();
     }
 
+    /**
+     *  Getter for camera
+     * @return Camera
+     */
     public Camera getCamera() {
         return camera;
     }
 
+    /**
+     *  Setter for camera
+     * @param camera a Camera
+     */
     public void setCamera(Camera camera) {
         this.camera = camera;
     }
 
+    /**
+     *  Getter for width
+     * @return int
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     *  Setter for width
+     * @param width a int
+     */
     public void setWidth(int width) {
         this.width = width;
     }
 
+    /**
+     *  Getter for height
+     * @return int
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     *  Setter for height
+     * @param height a int
+     */
     public void setHeight(int height) {
         this.height = height;
     }
 
+    /**
+     *  Getter for lights
+     * @return ArrayList of Light
+     */
     public ArrayList<Light> getLights() {
         return lights;
     }
 
+    /**
+     *  Setter for lights
+     * @param lights ArrayList of Light
+     */
     public void setLights(ArrayList<Light> lights) {
         this.lights = lights;
     }
 
+    /**
+     *  Getter for shapes
+     * @return ArrayList of Shape
+     */
     public ArrayList<Shape> getShapes() {
         return shapes;
     }
 
+    /**
+     * Setter for shapes
+     * @param shapes ArrayList of Shape
+     */
     public void setShapes(ArrayList<Shape> shapes) {
         this.shapes = shapes;
     }
 
+    /**
+     *  Method to add a light item to the lights list
+     * @param light a Light
+     */
     public void addLight(Light light){
         this.lights.add(light);
     }
 
+    /**
+     *
+     Method to add a shape element to the shapes list
+     * @param shape a Shape
+     */
     public void addShape(Shape shape){
         this.shapes.add(shape);
     }
