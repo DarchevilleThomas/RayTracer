69f8fd9335de7f03cf771d456c571bf8aa8d7abb
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-24 09:39:13 +0200

    Color class update

--- src/main/java/fr/saeS3A01/group1/Color.java
+++ src/main/java/fr/saeS3A01/group1/Color.java
@@ -1,72 +1,115 @@
 package fr.saeS3A01.group1;
 
+/**
+ * This class represents a color as a triplet of red, green, and blue
+ * components.
+ * Each component is represented as a double precision floating point number.
+ */
 public class Color {
-
     private Triplet triplet;
 
     /**
-     * Constructor of the triple corresponding to a color from the colors red, green and blue
-     * @param red a double
-     * @param green a double
-     * @param blue a double
-     */
-    public Color(double red, double green, double blue){
-        triplet = new Triplet(red,green,blue);
-    }
-
-    /**
-     *Constructor of the triple corresponding to a color from a triplet
-     * @param triplet a Triplet
-     */
-    public Color(Triplet triplet){
-        this(triplet.x, triplet.y, triplet.z);
-    }
-
-    /**
-     * Getter for triplet
-     * @return Triplet
+     * Returns the RGB triplet of this Color.
+     * 
+     * @return The RGB triplet of this Color.
      */
     public Triplet getTriplet() {
-        return triplet;
+        return this.triplet;
     }
 
     /**
-     * Setter for triplet
-     * @param triplet a Triplet
+     * Sets the RGB triplet of this Color to the RGB triplet of another Color.
+     * 
+     * @param color The other Color whose RGB triplet is to be copied.
      */
-    private void setTriplet(Triplet triplet) {
+    public void setTriplet(Color color) {
+        this.triplet = color.getTriplet();
+    }
+
+    /**
+     * Sets the RGB triplet of this Color.
+     * 
+     * @param triplet The new RGB triplet for this Color.
+     */
+    public void setTriplet(Triplet triplet) {
         this.triplet = triplet;
     }
 
     /**
-     * Method for adding color
-     * @param red a double
-     * @param green a double
-     * @param blue a double
-     * @return Color
+     * Sets the RGB triplet of this Color using individual red, green, and blue
+     * values.
+     * 
+     * @param red   The red component of the new RGB triplet.
+     * @param green The green component of the new RGB triplet.
+     * @param blue  The blue component of the new RGB triplet.
      */
-    public Color add(double red, double green, double blue){
-        return new Color(triplet.add(new Triplet(red,green,blue)));
+    public void setTriplet(double red, double green, double blue) {
+        this.triplet = new Triplet(red, green, blue);
     }
 
     /**
-     * Method for multiplication by a scalar
-     * @param d a double
-     * @return Color
+     * Constructs a new Color with the given red, green, and blue values.
+     * 
+     * @param red   The red component of the new Color.
+     * @param green The green component of the new Color.
+     * @param blue  The blue component of the new Color.
      */
-    public Color mul(double d){
-        return new Color(triplet.mul(d));
+    public Color(double red, double green, double blue) {
+        this.triplet = new Triplet(red, green, blue);
     }
 
     /**
-     * Method for Schur product
-     * @param red a double
-     * @param green a double
-     * @param blue a double
-     * @return Color
+     * Constructs a new Color with the given RGB triplet.
+     * 
+     * @param triplet The RGB triplet for the new Color.
      */
-    public Color schur(double red, double green, double blue){
-        return new Color(triplet.schur(new Triplet(red,green,blue)));
+    public Color(Triplet triplet) {
+        this.triplet = triplet;
     }
 
+    /**
+     * Constructs a new Color by copying the RGB triplet from another Color.
+     * 
+     * @param color The other Color whose RGB triplet is to be copied.
+     */
+    public Color(Color color) {
+        this.triplet = color.getTriplet();
+    }
+
+    /**
+     * Returns a new Color that is the product of this Color and a scalar.
+     * Each component (red, green, and blue) of the original color is multiplied by
+     * the scalar to produce the new color.
+     * 
+     * @param scalar The scalar to multiply with.
+     * @return A new Color that is the product of this Color and a scalar.
+     */
+    public Color multiply(double scalar) {
+        return new Color(this.triplet.mul(scalar));
+    }
+
+    /**
+     * Returns a new Color that is the sum of this and another Color.
+     * Each corresponding component (red, green, and blue) from both colors are
+     * added together to produce the new color.
+     * 
+     * @param other The other color to add with.
+     * @return A new color that is the sum of this and another color.
+     */
+    public Color add(Color other) {
+        return new Color(this.triplet.add(other.getTriplet()));
+    }
+
+    /**
+     * Returns a new color that is the Schur product (element-wise multiplication)
+     * of this and another color.
+     * Each corresponding component (red, green, and blue) from both colors are
+     * multiplied together to produce the new color.
+     * 
+     * @param other The other color to perform Schur product with.
+     * @return A new color that is the Schur product of this and another color.
+     */
+    public Color schurProduct(Color other) {
+        return new Color(this.triplet.schur(other.getTriplet()));
+    }
 }
