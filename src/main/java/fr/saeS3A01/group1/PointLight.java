6f0c064ca42fea0f3c18a0c7e7734cb1fda546d8
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-24 10:45:19 +0200

    lithning update

--- src/main/java/fr/saeS3A01/group1/PointLight.java
+++ src/main/java/fr/saeS3A01/group1/PointLight.java
@@ -2,10 +2,8 @@ package fr.saeS3A01.group1;
 
 /**
  * This class represents a PointLight which is a type of Light.
- * It has an additional property 'intensity' represented by a Vector.
  */
 class PointLight extends Light {
-    protected Vector intensity;
 
     /**
      * Constructs a new PointLight with the specified color, position, and intensity.
@@ -14,17 +12,7 @@ class PointLight extends Light {
      * @param position The position of the light.
      * @param intensity The intensity of the light.
      */
-    PointLight(Color color, Point position, Vector intensity) {
+    PointLight(Color color, Point position) {
         super(position, color);
-        this.intensity = intensity;
-    }
-
-    /**
-     * Returns the intensity of this PointLight.
-     *
-     * @return The intensity of this PointLight.
-     */
-    public Vector getIntensity() {
-        return this.intensity;
     }
 }
