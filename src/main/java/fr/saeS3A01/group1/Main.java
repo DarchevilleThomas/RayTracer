3e54ba74150a6813221e7a2f59f0f73499e144f7
Thomas <darcheville.toma@gmail.com>
2023-10-24 11:12:39 +0200

    Read parameters (Recovering file name)

--- src/main/java/fr/saeS3A01/group1/Main.java
+++ src/main/java/fr/saeS3A01/group1/Main.java
@@ -2,6 +2,13 @@ package fr.saeS3A01.group1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length<1){
+            System.err.println("Enter at list one parameter (the file name)");
+        }
+        else{
+            String filename = args[0];
+        }
+
+
     }
 }
