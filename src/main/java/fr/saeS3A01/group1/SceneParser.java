a7011c932809c265eee0485f00a81ca6359ceb93
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-24 10:33:11 +0200

    Creating a SceneParser.java file and his class with attribut and assessor

---
+++ src/main/java/fr/saeS3A01/group1/SceneParser.java
@@ -0,0 +1,19 @@
+package fr.saeS3A01.group1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SceneParser {
+    private String output;
+
+    public SceneParser() {
+        this.output = "";
+    }
+    
+    public String getOutput() {
+        return this.output;
+    }
+}
