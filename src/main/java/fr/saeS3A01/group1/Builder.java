dc72bcdc5286363089c1b20809e21d9bcb3f8dd7
Noah <hemery.noah18@gmail.com>
2023-10-24 10:19:10 +0200

    Added an interface Builder and its methods

--- src/main/java/fr/saeS3A01/group1/Builder.java
+++ src/main/java/fr/saeS3A01/group1/Builder.java
@@ -6,9 +6,34 @@ import java.util.ArrayList;
  * Builder interface defines all possible ways to configure a product.
  */
 public interface Builder {
+
+    /**
+     * Method to set a new camera.
+     * @param camera
+     */
     void setCamera(Camera camera);
+
+    /**
+     * Method to set a new width
+     * @param width
+     */
     void setWidth(int width);
+
+    /**
+     * Method to set a new height
+     * @param height
+     */
     void setHeight(int height);
+
+    /**
+     * Method to set a new list of lights
+     * @param lights
+     */
     void setLights(ArrayList<Light> lights);
+
+    /**
+     * Method to set a new list of shapes
+     * @param shapes
+     */
     void setShapes(ArrayList<Shape> shapes);
 }
