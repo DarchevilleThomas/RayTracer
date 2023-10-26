9d158db6b4e21002b3bccec899f3d375136676be
Gregory <gregory>
2023-10-26 10:54:43 +0200

    ne pas push

--- src/main/java/fr/saeS3A01/group1/LambertStrategy.java
+++ src/main/java/fr/saeS3A01/group1/LambertStrategy.java
@@ -20,27 +20,19 @@ public class LambertStrategy implements ColorStrategy{
         }
 
         else if (shape instanceof Triangle) {
-            n = ((((Triangle) shape).getPoint2().sub(((Triangle) shape).getPoint3())).cross((((Triangle) shape).getPoint1().sub(((Triangle) shape).getPoint3())))).normalize();
-            Plane plane = new Plane(shape.getDiffuse(),shape.getSpecular(),shape.getShininess(),((Triangle) shape).getPoint1(),n);
-            double t = plane.distance(scene.getCamera().getPosition(),d);
-            Point pPlane = d.mul(t).add(scene.getCamera().getPosition());
-            if( !(n.dot(((Triangle) shape).getPoint2().sub(((Triangle) shape).getPoint1()).cross(pPlane.sub(((Triangle) shape).getPoint1())))>= 0)
-                    || !(n.dot(((Triangle) shape).getPoint3().sub(((Triangle) shape).getPoint2()).cross(pPlane.sub(((Triangle) shape).getPoint2())))>= 0)
-                    || !(n.dot(((Triangle) shape).getPoint1().sub(((Triangle) shape).getPoint3()).cross(pPlane.sub(((Triangle) shape).getPoint3())))>= 0)) {
-                return new Color(0,0,0);
-            }
+            n = (p.sub(((Triangle) shape).getPoint1())).normalize();
+
         }
 
         else if (shape instanceof Plane) {
-            double t = shape.distance(scene.getCamera().getPosition(),d);
-            Point pPlane = d.mul(t).add(scene.getCamera().getPosition());
-            n = (pPlane.sub(((Plane) shape).getPoint())).normalize();
+            n = (p.sub(((Plane) shape).getPoint())).normalize();
         }
 
         if (scene.getAmbient() == null) res = new Color(0, 0, 0);
         else res = scene.getAmbient();
         for (Light light : lights) {
-            if (light instanceof PointLight plight) {
+            if (light instanceof PointLight) {
+                PointLight plight = (PointLight) light;
                 res = res.add(shape.getDiffuse().schurProduct(light.getColor().multiply(max(n.dot(plight.getPointLightVector(p)), 0))));
             } else {
                 DirectionalLight dlight = (DirectionalLight) light;
