2f739a54a3e31df771c7e29dce50b33681a03b9e
Gregory <gregory>
2023-10-26 18:28:38 +0200

    fix Sonarlint in Builder Interface

--- src/main/java/fr/saeS3A01/group1/Builder.java
+++ src/main/java/fr/saeS3A01/group1/Builder.java
@@ -1,7 +1,5 @@
 package fr.saeS3A01.group1;
 
-import java.util.ArrayList;
-
 /**
  * Builder interface defines all possible ways to configure a product.
  */
@@ -9,37 +7,37 @@ public interface Builder {
 
     /**
      * Method to set a new camera.
-     * @param camera
+     * @param camera a Camera
      */
     void setCamera(Camera camera);
 
     /**
      * Method to set a new ambient color.
-     * @param ambient
+     * @param ambient a Color
      */
     void setAmbient(Color ambient);
 
     /**
      * Method to set a new width
-     * @param width
+     * @param width int
      */
     void setWidth(int width);
 
     /**
      * Method to set a new height
-     * @param height
+     * @param height int
      */
     void setHeight(int height);
 
     /**
      * Method to add a new light in the list of lights.
-     * @param light
+     * @param light a Light
      */
     void addLight(Light light);
 
     /**
      * Method to add a new shape in the list of shapes.
-     * @param shape
+     * @param shape a Shape
      */
     void addShape(Shape shape);
 }
